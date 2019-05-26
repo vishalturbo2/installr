f074218fe604de05ba9c31f50058ad8155cbbae6
vishalturbo2 <42466022+vishalturbo2@users.noreply.github.com>
2019-05-26 08:09:48 +0530

    Update docker.sh

--- files/docker.sh
+++ files/docker.sh
@@ -1,1 +1,21 @@
 
+#!/bin/bash -e
+clear
+echo "============================================"
+echo "============================================"
+echo "docker installation script by Team Codeskulls"
+echo "============================================"
+echo "============================================"
+apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+echo "adding key"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "adding repository"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo "updating"
+sudo apt-get update
+echo "installing docker"
+sudo apt-get install -y docker-ce
+echo "testing docker"
+docker run hello-world
+exit
