fce8cb80f55ef6564ca887d48896bba070b7e987
vishalturbo2 <42466022+vishalturbo2@users.noreply.github.com>
2019-05-26 08:16:52 +0530

    Update jenkins.sh

--- files/jenkins.sh
+++ files/jenkins.sh
@@ -1,1 +1,21 @@
-
+#!/bin/bash -e
+clear
+echo "============================================"
+echo "============================================"
+echo "jenkins installation script by Team Codeskulls"
+echo "============================================"
+echo "============================================"
+apt-get update
+echo "downloading jdk"
+apt-get update
+sudo apt-get install openjdk-8-jdk
+echo "installing web server"
+sudo apt-get install apache2
+echo "adding keys"
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+echo "ainstalling jenkins"
+sudo apt-get update
+sudo apt-get install jenkins
+ufw allow 8080
+echo "jenkins installed successfully"
